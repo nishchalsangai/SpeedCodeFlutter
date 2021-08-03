7263f0e9e04dc9fb7403fb29a563bc4027d1d52f
nishchalsangai <nishchalsangai@gmail.com>
2021-08-03 21:42:43 +0530

    Whatsapp UI Speed Code done

---
+++ whatsapp_speed_code/lib/data.dart
@@ -0,0 +1,49 @@
+import 'package:whatsapp_speed_code/main.dart';
+
+final contentData = [
+  ScreenContent(
+      imgUrl: 'https://media.giphy.com/media/G3FNI3FneNjiw/giphy.gif',
+      message: 'Believe me, You can do it!',
+      name: 'Naruto',
+      time: '6:15 am'),
+  ScreenContent(
+      imgUrl: 'https://media.giphy.com/media/AkRFIhfAKHsyc/giphy.gif',
+      message: "I don't particularly like anything.",
+      name: 'Sasuke',
+      time: '6:20 am'),
+  ScreenContent(
+      imgUrl: 'https://media.giphy.com/media/aY1HMl4E1Ju1y/giphy.gif',
+      message: "I'm the sensei.",
+      name: 'Kakashi',
+      time: '6:25 am'),
+  ScreenContent(
+      imgUrl: 'https://media.giphy.com/media/ulB88Gahq9dBu/giphy.gif',
+      message: "I love you Sasuke.",
+      name: 'Sakura',
+      time: '6:30 am'),
+  ScreenContent(
+      imgUrl: 'https://media.giphy.com/media/7BW9U2cJPQZ0s/giphy.gif',
+      message: "I'm lufi.",
+      name: 'Lufi',
+      time: '6:40 am'),
+  ScreenContent(
+      imgUrl: 'https://media.giphy.com/media/3tjphbjcACUuc/giphy.gif',
+      message: "I'm son of the Fire Dragon King",
+      name: 'Natsu Dragneel',
+      time: '6:45 am'),
+  ScreenContent(
+      imgUrl: 'https://media.giphy.com/media/pD0mWWXxbd3gI/giphy.gif',
+      message: "My best Friend is Natsu",
+      name: 'Happy',
+      time: '6:50 am'),
+  ScreenContent(
+      imgUrl: 'https://media.giphy.com/media/fvcrSYkOmtP8c/giphy.gif',
+      message: "Natsu, let's see how's stronger ",
+      name: 'Gray',
+      time: '6:45 am'),
+  ScreenContent(
+      imgUrl: 'https://media.giphy.com/media/1forTCExMq1F3M2JjS/giphy.gif',
+      message: "If I can't use magic then I'll nullify it",
+      name: 'Asta',
+      time: '6:50 am'),
+];
--- whatsapp_speed_code/lib/main.dart
+++ whatsapp_speed_code/lib/main.dart
@@ -1,43 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_speed_code/data.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Whatsapp Ui',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home:
+          DefaultTabController(length: 4, child: MyHomePage(title: 'WhatsApp')),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -46,68 +29,107 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Color(0xff25D366),
+        child: Icon(Icons.chat),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Color(0xff075e54),
+        bottom: TabBar(
+          indicatorColor: Colors.white,
+          indicatorWeight: 3.0,
+          tabs: [
+            Tab(
+              icon: Icon(Icons.camera_alt_rounded),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Tab(
+              child: Text(
+                "CHATS",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w800),
+              ),
+            ),
+            Tab(
+              child: Text(
+                "STATUS",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w800),
+              ),
+            ),
+            Tab(
+              child: Text(
+                "CALLS",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w800),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: ListView(
+        itemExtent: 90.0,
+        children: contentData,
+      ),
+    );
+  }
+}
+
+class ScreenContent extends StatelessWidget {
+  final String? imgUrl;
+  final String? name;
+  final String? message;
+  final String? time;
+  const ScreenContent(
+      {Key? key, this.imgUrl, this.name, this.message, this.time})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 30.0,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Image.network(
+                  imgUrl!,
+                  width: 200,
+                  height: 200,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            title: Text(
+              name!,
+              style: TextStyle(fontSize: 17, fontWeight: FontWeight.w800),
+            ),
+            subtitle: Text(message!),
+            trailing: Column(
+              children: [
+                Container(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Text(
+                      time!,
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.grey.shade600),
+                    )),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 85, right: 10),
+          child: Divider(
+            thickness: 0.5,
+          ),
+        ),
+      ],
     );
   }
 }
