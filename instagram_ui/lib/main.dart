a3098e7d8c382de5414ce95cef75d431424dede0
nishchalsangai <nishchalsangai@gmail.com>
2021-08-04 18:52:59 +0530

    Some changes

--- instagram_ui/lib/main.dart
+++ instagram_ui/lib/main.dart
@@ -1,8 +1,6 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:instagram_ui/data.dart';
 import 'package:line_icons/line_icons.dart';
 
